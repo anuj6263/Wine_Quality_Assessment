85347778e5a50a0d279cc2d622569346008d757b
anuj6263 <anuj.shrivatri21@gmail.com>
2023-11-28 16:01:15 +0530

    different models with scores -added

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
+import os
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/anuj6263/Wine_Quality_Assessment.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="anuj6263"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="a183c07644e3b03d7081a2bd0eea5eaa843d61ef"
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -56,13 +61,13 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Model evaluation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = ModelEvaluationTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
